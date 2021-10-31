fbe296a129e499c12952d5746935bd08323775f3
RizvanGaripov <rizvangaripov@gmail.com>
2021-10-31 10:30:01 +0300

    added navigation-component

--- app/src/main/java/com/example/workforfirstsem/MainActivity.kt
+++ app/src/main/java/com/example/workforfirstsem/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.workforfirstsem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val controller = (supportFragmentManager.findFragmentById(R.id.container)
+                as NavHostFragment).navController
         setContentView(R.layout.activity_main)
     }
 }
